1802688e8739340edf5c15322292a7de2927ad48
CliffyA <github@cliffya.com>
2025-05-05 17:28:15 +1000

    initial commit

---
+++ ChartExample/ChartExample.cpp
@@ -0,0 +1,69 @@
+#include "../NumberDuck.hpp"
+
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace NumberDuck;
+
+int main(int argc, char **argv)
+{
+	printf("Chart Example\n");
+	printf("Embed a chart in a spreadsheet!\n\n");
+	
+	Workbook* pWorkbook = new Workbook();
+	Worksheet* pWorksheet = pWorkbook->GetWorksheetByIndex(0);
+
+	Cell* pCell = pWorksheet->GetCellByAddress("A1");
+	pCell->SetString("Duck Productivity");
+	
+	// filling out the table of data
+		pWorksheet->GetCellByAddress("B2")->SetString("Quacks");
+		pWorksheet->GetCellByAddress("C2")->SetString("Waddles");
+		
+		int nQuacks = 90;
+		int nWaddles = 70;	
+		for (int i = 0; i <  9; i++)
+		{
+			char szTemp[100];
+			sprintf(szTemp,"Day %d", i+1);
+			nQuacks += int(rand()%50);
+			nWaddles += int(rand()%50);
+			
+			pWorksheet->GetCell(0, i+2)->SetString(szTemp);
+			pWorksheet->GetCell(1, i+2)->SetFloat(nQuacks);
+			pWorksheet->GetCell(2, i+2)->SetFloat(nWaddles);
+		}
+		
+	// create the chart
+	Chart* pChart = pWorksheet->CreateChart(Chart::TYPE_COLUMN);
+	
+	// set cell position for the chart
+	pChart->SetX(3);
+	pChart->SetY(1);
+	
+	// center the chart within the cell
+	pChart->SetSubX(Worksheet::DEFAULT_COLUMN_WIDTH / 2);
+	pChart->SetSubY(Worksheet::DEFAULT_ROW_HEIGHT / 2);
+	
+	pChart->SetWidth(Worksheet::DEFAULT_COLUMN_WIDTH*5);
+	pChart->SetHeight(Worksheet::DEFAULT_ROW_HEIGHT*9);
+	
+	pChart->SetCategories("=A3:A11");
+	
+	// Create the 'quacks' series
+	Series* pSeries = pChart->CreateSeries("=B3:B11");
+	pSeries->SetName("=B2");
+	
+	// Create the 'waddles' series
+	pSeries = pChart->CreateSeries("=C3:C11");
+	pSeries->SetName("=C2");
+	
+	
+	pWorkbook->Save("ChartExample.xls", Workbook::FILE_TYPE_XLS);
+
+	delete pWorkbook;
+
+	return 0;
+}
+
---
+++ FormulaExample/FormulaExample.cpp
@@ -0,0 +1,42 @@
+#include "../NumberDuck.hpp"
+#include <stdio.h>
+
+using namespace NumberDuck;
+
+int main(int argc, char **argv)
+{
+	printf("Formula Example\n");
+	printf("Create a spreadsheet with formulas!\n\n");
+	
+	Workbook* pWorkbook = new Workbook();
+	Worksheet* pWorksheet = pWorkbook->GetWorksheetByIndex(0);
+		
+	for (int i = 0; i < 5; i++)
+	{
+		Cell* pCell = pWorksheet->GetCell(0, i);
+		pCell->SetFloat(i * 2.34f);
+	}
+
+	pWorksheet->GetCell(1,0)->SetString("=SUM(A1:A5)");
+	pWorksheet->GetCell(1,1)->SetString("=AVERAGE(A1:A5)");
+
+	pWorksheet->GetCell(2,0)->SetFormula("=SUM(A1:A5)");
+	pWorksheet->GetCell(2,1)->SetFormula("=AVERAGE(A1:A5)");
+		
+	pWorkbook->Save("FormulaExample.xls", Workbook::FILE_TYPE_XLS);
+
+	delete pWorkbook;
+
+
+	Workbook* pWorkbookIn = new Workbook();
+	if (pWorkbookIn->Load("FormulaExample.xls"))
+	{
+		Worksheet* pWorksheetIn = pWorkbookIn->GetWorksheetByIndex(0);
+		Cell* pCellIn = pWorksheetIn->GetCell(2,1);
+		printf("Formula: %s\n", pCellIn->GetFormula());
+	}
+	delete pWorkbookIn;
+
+	return 0;
+}
+
